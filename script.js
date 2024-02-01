4f8087a0ad51a5ad207db7ac75e3f6836530a6de
krishna-javvaji <kjavvaji@uncc.edu>
2024-02-01 18:14:32 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example JavaScript to toggle a class on an element
+function toggleClass(elementId, className) {
+    var element = document.getElementById(elementId);
+    element.classList.toggle(className);
+}
